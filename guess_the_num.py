046fca71a0316ca4679506b6c59401931d49ff53
jakeinabox <98402073+jakeinab0x@users.noreply.github.com>
2022-04-25 22:43:33 +0100

    git commit

--- guess_the_num.py
+++ guess_the_num.py
@@ -11,4 +11,19 @@ def guess(x):
             print("Sorry, guess again. Too high.")
     print(f"Yay! Congrats, you have guessed the number {random_number} correctly!")
             
-guess(10)
+# guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        guess = random.randint(low, high)
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)?".lower())
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f"Yay! The computer guessed your number, {guess}, correctly.")
+
+computer_guess(1)
